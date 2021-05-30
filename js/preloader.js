5fb9b56b8ba645e6f619041de62faecb7545f2e5
FelixBoiii <47904281+FelixBoiii@users.noreply.github.com>
2021-05-30 21:55:09 +0200

    beterrrrr

--- js/preloader.js
+++ js/preloader.js
@@ -1,4 +1,18 @@
-$(window).load(function() {
+$(window).load(function () {
   // Animate loader off screen
   $(".se-pre-con").fadeOut("slow");
+
+
+  setTimeout(function () {
+    $("#cookieConsent").fadeIn(200);
+  }, 500);
+
+  $("#closeCookieConsent, .cookieConsentOK").click(function () {
+    $("#cookieConsent2").fadeOut(1);
+    $("#cookieConsent").fadeOut(200);
+  });
+
+  $("#closeCookieConsent, .cookieConsentNOT").click(function () {
+    $("#cookieConsent").fadeOut(10);
+  });
 });
