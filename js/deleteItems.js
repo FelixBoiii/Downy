27bc0e3426649a549e3fc21be07e35c45b6f86d4
FelixBoiii <47904281+FelixBoiii@users.noreply.github.com>
2021-06-13 15:44:55 +0200

    gg

--- js/deleteItems.js
+++ js/deleteItems.js
@@ -1,34 +1,16 @@
-$(function() {
-  $("#remove").click(function() {
-    var confirm = window.confirm("Are you sure you want to delete this item?");
-    if (confirm == true) {
-      $("#item").hide();
-      alert("Item deleted!");
-    } else {
-      alert("Item not deleted");
-    }
+$(function () {
+  $("#remove").click(function () {
+    $("#item").hide();
   });
-  $("#remove2").click(function() {
-    var confirm = window.confirm("Are you sure you want to delete this item?");
-    if (confirm == true) {
-      $("#item1").hide();
-      alert("Item deleted!");
-    } else {
-      alert("Item not deleted");
-    }
+  $("#remove2").click(function () {
+    $("#item1").hide();
   });
-  $("#remove3").click(function() {
-    var confirm = window.confirm("Are you sure you want to delete this item?");
-    if (confirm == true) {
-      $("#item2").hide();
-      alert("Item deleted!");
-    } else {
-      alert("Item not deleted");
-    }
+  $("#remove3").click(function () {
+    $("#item2").hide();
     $("hr").hide();
   });
 
-  $("[type='number']").keypress(function(evt) {
+  $("[type='number']").keypress(function (evt) {
     evt.preventDefault();
     alert("Please use the up and down arrows to change the values!");
   });
--- js/preloader.js
+++ js/preloader.js
@@ -15,4 +15,27 @@ $(window).load(function () {
   $("#closeCookieConsent, .cookieConsentNOT").click(function () {
     $("#cookieConsent").fadeOut(10);
   });
+
+  $("#closeCookieConsent, #cookieConsentnee").click(function () {
+    $("#cookieConsent2").fadeOut(1);
+    $("#cookieConsent").fadeOut(200);
+  });
+
+  //timer
+  var countDownDate = new Date(new Date().getTime() + 7 * 60 * 60 * 1000);
+  var x = setInterval(function () {
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("timer").innerHTML = hours + "h "
+      + minutes + "m " + seconds + "s ";
+
+  }, 1000);
+  //
 });
